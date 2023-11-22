71df3f7e556c9c7534f1406055b7463468294041
Pavel <pavel.timofeev2008@gmail.com>
2023-11-22 13:08:24 -0800

    little tkinter commit number two

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ window = tk.Tk()
 window.title('Calculator')
 WINDOW_MAX_WIDTH = 450
 WINDOW_MAX_HEIGHT = 450
+DEFAULT_IMAGE_BUTTON_WIDTH = 10
+DEFAULT_IMAGE_BUTTON_HEIGTH = 10
 window.geometry(str(WINDOW_MAX_WIDTH)+"x"+str(WINDOW_MAX_HEIGHT))
 mtrx_of_symb = [['1', '2', '3', '+'],
                ['4', '5','6','-'],
@@ -23,11 +25,11 @@ def create_button(symbol):
         x_pos = 10
         y_pos+=30*i
         for j in range(len(mtrx_of_symb[i])):                  
-            btn = tk.Button(window, text=mtrx_of_symb[i][j], width=1, height=1)
+            btn = tk.Button(window, text=mtrx_of_symb[i][j], width=DEFAULT_IMAGE_BUTTON_WIDTH, height=DEFAULT_IMAGE_BUTTON_HEIGTH)
             btn.pack() 
             x_pos += 50*j
             btn.place(x=x_pos, y=y_pos)
-btn = tk.Button(window, image=click_btn, width=10, height=10)
+btn = tk.Button(window, image=click_btn, width=DEFAULT_IMAGE_BUTTON_WIDTH, height=DEFAULT_IMAGE_BUTTON_HEIGTH)
 btn.pack()
 btn.place(x=150,y=150)
 window.mainloop()
