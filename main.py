d9c347c104facd25ee549fe8c596a9bc46a4469c
Pavel <pavel.timofeev2008@gmail.com>
2023-11-27 20:16:17 -0800

    refactoring code

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import tkinter as tk
+import re
+from constants import *
 
 window = tk.Tk()
 window.title('Calculator')
-WINDOW_MAX_WIDTH = 200
-WINDOW_MAX_HEIGHT = 300
-DEFAULT_IMAGE_BUTTON_WIDTH = 20
-DEFAULT_IMAGE_BUTTON_HEIGHT = 30
+
 window.geometry(str(WINDOW_MAX_WIDTH) + "x" + str(WINDOW_MAX_HEIGHT))
 window.resizable(0, 0)
+ALLOWED_IN_BUFFER_REGEX_PATTERN = re.compile(r'[\d\-\+\*/]')
 
 mtrx_of_symb = [['one_resized.png', 'two_resized.png', 'three_resized.png', 'plus_sign.png'],
                 ['four_resized.png', 'five_resized.png', 'six_resized.png', 'minus_sign.png'],
@@ -15,141 +15,112 @@ mtrx_of_symb = [['one_resized.png', 'two_resized.png', 'three_resized.png', 'plu
                 ['AC.png', 'zero_resized.png', 'equal_sign.png', 'devision_sign.png'],
                 ["nothing.png", "nothing.png", "nothing.png", "nothing.png"]]
 
+# def create_matrix(matrix_rows, matrix_columns, map):
+    
+
+
 button_frame = tk.Frame(window, width=200, height=300, highlightbackground="gold", bg="cyan4", highlightthickness=3)
 button_frame.place(x=0, y=0)
 
-string = tk.StringVar(window)
+main_window_display_string = tk.StringVar(window)
 
-entry = tk.Entry(window, textvariable=string, width=20, bg="cyan4", justify='right')
-entry.place(x=4, y=4)
+main_display_entry = tk.Entry(window, textvariable=main_window_display_string, width=20, bg="cyan4", justify='right')
+main_display_entry.place(x=4, y=4)
 
-def update_entry(symb):
-    current_value = string.get()
-    new_value = current_value + symb
-    string.set(new_value)
+def update_entry(button_sign_text):
+    existing_number_buffer = main_window_display_string.get()
+    new_buffer = ''
+    
+    if (button_sign_text == EQUAL_SIGN):
+        new_buffer = evaluate_buffer()
+    elif (button_sign_text == "AC"):
+        clear_entry()
+    elif re.search(ALLOWED_IN_BUFFER_REGEX_PATTERN, button_sign_text):
+        new_buffer = existing_number_buffer + button_sign_text
+    else:
+        pass
+    
+    main_window_display_string.set(new_buffer)
 
 def clear_entry():
-    string.set("")
+    main_window_display_string.set("")
 
-def counting():
+def evaluate_buffer():
     try:
-        expression = string.get()
-        if "÷" in expression:
-            expression = expression.replace("÷", "/")
-        result = str(eval(expression))
-        string.set(result)
+        expression_to_evaluate = main_window_display_string.get()
+        result = str(eval(expression_to_evaluate))
+        return result
     except Exception as e:
-        string.set("Error")
-
-def btn_clck(c_symb):
-    match(c_symb):
-        case "zero.resized.png":
-            c_symb="0"
-            update_entry(c_symb)
-        case "one_resized.png":
-            c_symb = "1"
-            update_entry(c_symb)
-        case "two_resized.png":
-            c_symb = "2"
-            update_entry(c_symb)
-        case "three_resized.png":
-            c_symb = "3"
-            update_entry(c_symb)
-        case "four_resized.png":
-            c_symb = "4"
-            update_entry(c_symb)
-        case "five_resized.png":
-            c_symb = "5"
-            update_entry(c_symb)
-        case "six_resized.png":
-            c_symb = "6"
-            update_entry(c_symb)
-        case "seven_resized.png":
-            c_symb = "7"
-            update_entry(c_symb)
-        case "eight_resized.png":
-            c_symb = "8"
-            update_entry(c_symb)
-        case "nine_resized.png":
-            c_symb = "9"
-            update_entry(c_symb)
-        case "plus_sign.png":
-            c_symb = "+"
-            update_entry(c_symb)
-        case "minus_sign.png":
-            c_symb = "-"
-            update_entry(c_symb)
-        case "multiplication_sign.png":
-            c_symb = "*"
-            update_entry(c_symb)
-        case "devision_sign.png":
-            c_symb = "÷"
-            update_entry(c_symb)
-        case "nothing.png":
-            c_symb = ""
-            update_entry(c_symb)
-        case "equal_sign.png":
-            counting()
-        case "AC.png":
-            clear_entry()
-
+        main_window_display_string.set("Error")
 
 def create_button(symb_matrix):
+    # FIXME: use os library to get current directory
     image_file_path = "/Users/pavtim127/Desktop/MyCalculatorNotJava228/symbols/"
-    x_pos = 15
+    # FIXME: all coordinate positions should be defined as constant variables
+    # e.g x_pos -> X_AXIS_OFFSET
     y_pos = 0
 
     for i in range(len(symb_matrix)):
         x_pos = 15
         y_pos += i + 48
         for j in range(len(symb_matrix[i])):
-            img = tk.PhotoImage(file=image_file_path + symb_matrix[i][j])
-            current_symb = symb_matrix[i][j]            
+            button_image = tk.PhotoImage(file=image_file_path + symb_matrix[i][j])
+            current_symb = symb_matrix[i][j]
+
+            # FIXME: this string cutting is too complicated, simlify
             sign = current_symb.split('.')[0].replace('_resized', '').replace('_', ' ')
-            match(sign):
-                case "one":
-                    sign = "1"
-                case "two":
-                    sign = "2"
-                case "three":
-                    sign = "3"
-                case "four":
-                    sign = "4"
-                case "five":
-                    sign = "5"
-                case "six":
-                    sign = "6"
-                case "seven":
-                    sign = "7"
-                case "eight":
-                    sign = "8"
-                case "nine":
-                    sign = "9"
-                case "zero":
-                    sign = "0"
-                case "plus sign":
-                    sign = "+"
-                case "minus sign":
-                    sign = "-"
-                case "multiplication sign":
-                    sign = "*"
-                case "devision sign":
-                    sign = "÷"
-                case "equal sign":
-                    sign = "="
-                case "nothing":
-                    sign=" "
-            btn = tk.Button(window, image=img, width=DEFAULT_IMAGE_BUTTON_WIDTH,
+
+            button_sign_text = getButtonSignText(sign)
+            
+            btn = tk.Button(window, image=button_image, width=DEFAULT_IMAGE_BUTTON_WIDTH,
                             height=DEFAULT_IMAGE_BUTTON_HEIGHT,
-                            text=sign,
+                            text=button_sign_text,
                             compound=tk.TOP,  
-                            command=lambda c_symb=current_symb: btn_clck(c_symb),
+                            command=lambda event, button_sign_text=button_sign_text: update_entry(button_sign_text),
                             relief=tk.GROOVE, 
                             font=('Arial', 17))
 
             btn.place(x=x_pos, y=y_pos)
             x_pos += 46 + j
-            btn.bind('<Button-1>', lambda event, current_symb=current_symb: btn_clck(current_symb))
+            btn.bind('<Button-1>', lambda event, button_sign_text=button_sign_text: update_entry(button_sign_text))
+
+def getButtonSignText(signToMatch):
+    match(signToMatch):
+        case "one":
+            return "1"
+        case "two":
+            return "2"
+        case "three":
+            return "3"
+        case "four":
+            return "4"
+        case "five":
+            return "5"
+        case "six":
+            return "6"
+        case "seven":
+            return "7"
+        case "eight":
+            return "8"
+        case "nine":
+            return "9"
+        case "zero":
+            return "0"
+        case "plus sign":
+            return "+"
+        case "minus sign":
+            return "-"
+        case "multiplication sign":
+            return "*"
+        case "devision sign":
+            return "/"
+        case "equal sign":
+            return "="
+        case "nothing":
+            return ""
+        case "AC":
+            return "AC"
+
 
 create_button(mtrx_of_symb)
 window.mainloop()
